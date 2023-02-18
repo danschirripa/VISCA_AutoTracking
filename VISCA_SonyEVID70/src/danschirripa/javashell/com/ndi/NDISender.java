355970fa30cba13ca779a3602e406dd4a7a7af94
Daniel <danschirripa98@gmail.com>
2023-02-17 22:46:48 -0500

    Fixed RPI issue with lib extraction, forced libs to be local for now

--- VISCA_SonyEVID70/src/danschirripa/javashell/com/ndi/NDISender.java
+++ VISCA_SonyEVID70/src/danschirripa/javashell/com/ndi/NDISender.java
@@ -1,27 +1,10 @@
 package danschirripa.javashell.com.ndi;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-
 public class NDISender {
 	static {
 		try {
-			InputStream libNdiStream = NDISender.class.getResourceAsStream("/libndi.so");
-			File libNdiFile = File.createTempFile("libndi", ".so");
-			FileOutputStream libNdiOut = new FileOutputStream(libNdiFile);
-			libNdiOut.write(libNdiStream.readAllBytes());
-			libNdiOut.flush();
-			libNdiOut.close();
-			System.load(libNdiFile.getAbsolutePath());
-
-			InputStream libNdiSenderStream = NDISender.class.getResourceAsStream("/libndisender.so");
-			File libNdiSenderFile = File.createTempFile("libndisender", ".so");
-			FileOutputStream libNdiSenderOut = new FileOutputStream(libNdiSenderFile);
-			libNdiSenderOut.write(libNdiSenderStream.readAllBytes());
-			libNdiSenderOut.flush();
-			libNdiSenderOut.close();
-			System.load(libNdiSenderFile.getAbsolutePath());
+			System.load("libndi.so");
+			System.load("libndisender.so");
 			System.out.println("LIBRARY LOAD COMPLETED");
 		} catch (Exception e) {
 			e.printStackTrace();
